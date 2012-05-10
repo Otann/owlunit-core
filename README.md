6d152eb7f73e9b0ea0389fbde99ad7bdb0e2909f
Anton Chebotaev <anton.chebotaev@gmail.com>
2012-05-11 01:27:31 +0400

    initial code moved from other repo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-core
-====
+# How to use
+
+1. Clone repository with `git clone git@github.com:OwlUnit/core.git`
+1. [Install SBT](https://github.com/harrah/xsbt/wiki/Getting-Started-Setup)
+1. Run in the bash/console `$ sbt`
+1. Initialize IntelliJ IDEA project with `> gen-idea` or `> gen-idea no-classifiers` to make things little happen little bit faster
+1. Compile in SBT with `> compile`
+1. Run tests in SBT with `> test`
+1. Publish artifact to local Ivy2 repository with `sbt publish-local`
---
+++ project/CoreBuild.scala
@@ -0,0 +1,55 @@
+package core
+
+import sbt._
+import Keys._
+import sbt.Package._
+import java.util.jar.Attributes.Name._
+
+object CoreBuild extends Build {
+
+  lazy val root = Project(
+    id = "core",
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(
+      resolvers ++= Seq("Neo4j Maven 2 release repository" at "http://m2.neo4j.org/releases"),
+      libraryDependencies ++= Seq(
+        Dependency.specs2,
+        Dependency.neo4j,
+        Dependency.neo4jREST
+        )
+      )
+    )
+
+  /////////////////////
+  // Settings
+  /////////////////////
+
+  override lazy val settings = super.settings ++ Seq(
+    organization := "com.owlunit",
+    version := "0.1-SNAPSHOT",
+    scalaVersion := "2.9.1",
+    resolvers ++= Seq(ScalaToolsSnapshots),
+    scalacOptions ++= Seq("-encoding", "UTF-8", "-deprecation", "-unchecked"),
+    javacOptions ++= Seq("-Xlint:unchecked"),
+    publishTo := Some(Resolver.file("file",  new File( "/Users/anton/Dev/Owls/repo/owlunit.github.com/repo/ivy/" )))
+  )
+
+
+  /////////////////////
+  // Dependencies
+  /////////////////////
+  
+  object Dependency {
+
+    val specs2      = "org.specs2"                %% "specs2"               % "1.9"    % "test"
+
+    val neo4j       = "org.neo4j"                 %  "neo4j"                % "1.6"
+    val neo4jREST   = "org.neo4j"                 %  "neo4j-rest-graphdb"   % "1.6"
+    // val neo4jRemote = "org.neo4j"                 %  "neo4j-remote-graphdb" % "0.7"
+
+    // val logback     = "ch.qos.logback"            %  "logback-classic"     % "0.9.28"
+    // val slf4s       = "com.weiglewilczek.slf4s"   %% "slf4s"               % "1.0.7"
+
+  }
+
+}
